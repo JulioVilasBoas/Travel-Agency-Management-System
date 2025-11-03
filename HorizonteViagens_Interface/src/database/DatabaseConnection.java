ad7777dc2c2737e935aab2b68aae103b7463e8aa
Júlio César Aragão Vilas Bôas <jcavilasboas@gmail.com>
2025-11-03 14:01:11 -0300

    pequeno ajuste
    ajuste no nome do bd, no java.

--- HorizonteViagens_Interface/src/database/DatabaseConnection.java
+++ HorizonteViagens_Interface/src/database/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     // Dados do banco de dados
-    private static final String URL = "jdbc:mysql://localhost:3306/projeto_bd";
+    private static final String URL = "jdbc:mysql://localhost:3306/mysql_database";
     private static final String USER = "root";
     private static final String PASSWORD = "root";
 
